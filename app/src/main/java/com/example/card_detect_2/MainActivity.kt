cff0466c302d92e78db46052764db799c6baf0b2
heabeounMKTO <heabeoun69zin@gmail.com>
2024-02-03 09:46:37 +0700

    yea

--- app/src/main/java/com/example/card_detect_2/MainActivity.kt
+++ app/src/main/java/com/example/card_detect_2/MainActivity.kt
@@ -14,18 +14,25 @@ import android.widget.ImageView
 import androidx.activity.*
 import androidx.activity.compose.setContent
 import androidx.annotation.RawRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.card_detect_2.ui.theme.Card_detect_2Theme
 import kotlinx.coroutines.*
 import java.util.*
 import java.io.InputStream
-
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     private var ortEnv: OrtEnvironment = OrtEnvironment.getEnvironment()
@@ -37,17 +44,24 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        inputImage = findViewById(R.id.imageView1)
-        outputImage = findViewById(R.id.imageView2)
-        objectDetectButton = findViewById(R.id.object_detection_button)
-        inputImage.setImageBitmap(
-            BitmapFactory.decodeStream(readInputImage(assets))
-        );
-        val sessionOptions: OrtSession.SessionOptions = OrtSession.SessionOptions()
-
-        classes = readClasses(resources)
+        setContent{
+            Card_detect_2Theme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background) {
+                    Greeting(name = "yes")
+                    GreetingImage()
+                }
+            }
+        }
+//        inputImage = findViewById(R.id.imageView1)
+//        outputImage = findViewById(R.id.imageView2)
+//        objectDetectButton = findViewById(R.id.object_detection_button)
+//        inputImage.setImageBitmap(
+//            BitmapFactory.decodeStream(readInputImage(assets))
+//        );
+//        val sessionOptions: OrtSession.SessionOptions = OrtSession.SessionOptions()
+//
+//        classes = readClasses(resources)
     }
 }
 
@@ -64,6 +78,14 @@ private fun readInputImage(assets: AssetManager): InputStream {
     return assets.open("frame_screenshot_29.01.2024.png")
 }
 
+
+@Composable
+fun GreetingImage() {
+    val image = painterResource(id = R.raw.test_object_detection_0)
+    Box(modifier=Modifier.padding(24.dp)) {
+        Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
+    }
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- app/src/main/java/com/example/card_detect_2/ObjectDetect.kt
+++ app/src/main/java/com/example/card_detect_2/ObjectDetect.kt
@@ -8,6 +8,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import java.io.InputStream
 import java.nio.ByteBuffer
+import java.util.Collections
 
 
 internal data class Result (
@@ -20,16 +21,28 @@ internal class ObjectDetect() {
     fun detect(inputStream: InputStream, ortEnv: OrtEnvironment, ortSession: OrtSession): Result {
         val rawImageBytes = inputStream.readBytes()
         val shape = longArrayOf(rawImageBytes.size.toLong())
-
+        // loads as uint8
         val inputTensor = OnnxTensor.createTensor(
             ortEnv,
             ByteBuffer.wrap(rawImageBytes),
             shape,
             OnnxJavaType.UINT8
         )
-       //  loads image as uint8 hehe
 
-        return TODO("Provide the return value")
+        inputTensor.use {
+            val output = ortSession.run(Collections.singletonMap("image", inputTensor),
+                setOf("image_out", "scaled_box_out_next")
+            )
+
+            output.use {
+                val rawOutput = (output?.get(0)?.value) as ByteArray
+                val boxOutput = (output?.get(1)?.value) as Array<FloatArray>
+                val outputImageBitmap = byteArrayToBitmap(rawOutput)
+
+                var result = Result(outputImageBitmap, boxOutput)
+                return result
+            }
+        }
     }
 }
 
